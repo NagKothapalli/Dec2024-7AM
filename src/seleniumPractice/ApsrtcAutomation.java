e1659cf6135f9d9eebf191eedc0950cc14fc1943
NagKothapalli <nag022@gmail.com>
2024-12-20 23:18:47 +0530

    Dec20th -2 code pushed

--- src/seleniumPractice/ApsrtcAutomation.java
+++ src/seleniumPractice/ApsrtcAutomation.java
@@ -11,10 +11,14 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ApsrtcAutomation
 {
 	ChromeDriver driver; // null
+	WebDriverWait  wait;
+	Actions actions;
 
 	@Before
 	public void launchApplication() throws InterruptedException {
@@ -23,13 +27,101 @@ public class ApsrtcAutomation
 										// chrome browser
 		driver.get("https://www.apsrtconline.in/");
 		driver.manage().window().maximize();
+		//driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		wait = new WebDriverWait(driver,Duration.ofSeconds(30));  // Explicit wait
+		actions = new Actions(driver);
 	}
 	
 	@Test
-	public void bookBusTicket()
+	public void bookBusTicketFlow() throws InterruptedException
 	{
 		System.out.println("Test Case : Book Bus Ticket");
 		driver.findElement(By.xpath("//input[@name='source']")).sendKeys("HYDERABAD");
+		Actions actions = new Actions(driver); //12345
+		actions.pause(Duration.ofSeconds(1)).sendKeys(Keys.ENTER).build().perform();
+		driver.findElement(By.xpath("//input[@name='destination']")).sendKeys("GUNTUR");
+		actions.pause(Duration.ofSeconds(1)).sendKeys(Keys.ENTER).build().perform();
+		driver.findElement(By.xpath("//input[@name='txtJourneyDate']")).click();
+		String dt = "26";  // With Date function in java get the current date and add some number 
+		//driver.findElement(By.xpath("//a[text()='25']")).click();
+		driver.findElement(By.xpath("//a[text()='"+dt+"']")).click();  // Dynamic xpath
+		driver.findElement(By.xpath("//input[@name='searchBtn']")).click();		
+	}
+	
+	//***********************************************************
+	
+	@Test
+	public void bookBusTicketFlow_Sync() throws InterruptedException
+	{
+		System.out.println("Test Case : Book Bus Ticket");
+		enterText("//input[@name='source']","HYDERABAD");
+		clickEnterButton();
+		enterText("//input[@name='destination']","GUNTUR");
+		clickEnterButton();
+		clickElement("//input[@name='txtJourneyDate']");
+		String dt = "26";  // With Date function in java get the current date and add some number 
+		clickElement("//a[text()='"+dt+"']");
+		clickElement("//input[@name='searchBtn']");		
+	}
+	
+	String fromCity = "//input[@name='source']";
+	String toCity = "//input[@name='destination']";
+	String openCalendar = "//input[@name='txtJourneyDate']";
+	String searchBtn = "//input[@name='searchBtn']";
+	
+	@Test
+	public void bookBusTicketFlow_Sync2() throws InterruptedException
+	{
+		System.out.println("Test Case : Book Bus Ticket");
+		enterText(fromCity,"HYDERABAD"); // Hard coded
+		clickEnterButton();
+		enterText(toCity,"GUNTUR"); // Hard coded
+		clickEnterButton();
+		clickElement(openCalendar);
+		String dt = "26";  // With Date function in java get the current date and add some number 
+		clickElement("//a[text()='"+dt+"']");
+		String txt = getWebElement(searchBtn).getText();
+		System.out.println(txt);
+		System.out.println(getWebElement(searchBtn).isDisplayed());
+		System.out.println(getWebElement(searchBtn).isEnabled());
+		System.out.println(getWebElement(searchBtn).getTagName());
+		System.out.println(getWebElement(searchBtn).getAttribute("value"));
+		clickElement(searchBtn);		
+	}
+	//-------------------------------------------------------------------------------
+	public WebElement getWebElement(String myxpath)
+	{
+		WebElement element = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(myxpath)));
+		return element;
+	}	
+	
+	public void clickElement(String myxpath)
+	{
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath(myxpath))).click();
+	}
+	
+	public void enterText(String myxpath, String text)
+	{
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath(myxpath))).sendKeys(text);
+	}
+	
+	public void clickEnterButton()
+	{
+		actions.pause(Duration.ofSeconds(1)).sendKeys(Keys.ENTER).build().perform();
+	}
+	
+	//************************************************************
+	
+	@Test
+	public void bookBusTicket() throws InterruptedException
+	{
+		System.out.println("Test Case : Book Bus Ticket");
+		//Thread.sleep(30000);  // Static wait / fixed wait 
+		//driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		//driver.findElement(By.xpath("//input[@name='source']")).sendKeys("HYDERABAD");
+		//WebElement sourceObj = driver.findElement(By.xpath("//input[@name='source']"));
+		//wait.until(ExpectedConditions.elementToBeClickable(sourceObj)).sendKeys("HYDERABAD");
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@name='source']"))).sendKeys("HYDERABAD");
 		Actions actions = new Actions(driver); //12345
 		actions.pause(Duration.ofSeconds(1)).sendKeys(Keys.ENTER).build().perform();		
 		driver.findElement(By.xpath("//input[@name='searchBtn']")).click();
@@ -69,7 +161,7 @@ public class ApsrtcAutomation
 		driver.findElement(By.xpath("//a[text()='All services Time Table & Tracking']")).click();
 		Set<String> windows = driver.getWindowHandles();
 		ArrayList<String> list = new ArrayList<String>(windows);
-		list.get(5);
+		//list.get(5);
 		for(String w : list) {
 			System.out.println(w);
 		}
@@ -78,10 +170,10 @@ public class ApsrtcAutomation
 		driver.switchTo().window(list.get(1));
 		Thread.sleep(2000);
 		System.out.println("Title of Second Window :" + driver.getTitle());
-		//driver.close(); // current active window will be closed
-		driver.quit();// it will kill the driver object - all windows will be closed
+		driver.close(); // current active window will be closed		
 		driver.switchTo().window(list.get(0));
 		driver.findElement(By.xpath("//a[@title='Home']")).click();
+		driver.quit();// it will kill the driver object - all windows will be closed
 		
 	}
 	
